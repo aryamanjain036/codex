ebdf4e4992f1dc985c39f02505c83f4101d6766b
Aryaman Jain <47586842+aryamanjain036@users.noreply.github.com>
2020-11-23 10:30:56 +0530

    updated q_16

--- jspd practice/chapter3/src/q_16.java
+++ jspd practice/chapter3/src/q_16.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class q_16 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int xCoordinate = (int)(Math.random()*(50+50) -50);
+        int yCoordinate = (int)(Math.random()*(100+100) -100);
+        System.out.println("the coordinate are " + "(" + xCoordinate + ", " + yCoordinate + ")");
+
     }
 }
