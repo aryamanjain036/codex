79687d648143934a0c8b74c8df33be55fa506cd1
Aryaman Jain <47586842+aryamanjain036@users.noreply.github.com>
2020-11-23 13:02:06 +0530

    updated q_27

--- jspd practice/chapter3/src/q_27.java
+++ jspd practice/chapter3/src/q_27.java
@@ -6,6 +6,18 @@ public class q_27 {
         System.out.println("Enter a point's x- and y- coordinates: ");
         double xCoordinate = scan.nextDouble();
         double yCoordinate = scan.nextDouble();
-
+        double midX = 200.0 / 2;
+        double midY = 100.0 / 2;
+        if (xCoordinate >= 200 && yCoordinate >= 100) {
+            System.out.println("The point is not in the triangle");
+        }
+        else {
+            if (xCoordinate > midX && yCoordinate > midY) {
+                System.out.println("The point is not in the triangle");
+            }
+            else {
+                System.out.println("The point is in the triangle");
+            }
+        }
     }
 }
